7ad8cbfb45ee104ede299360dfc13688911e1855
Richard Burd <richardburdx@yahoo.com>
2019-07-18 21:16:37 +0000

    Automatically backed up by Learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -21,7 +21,7 @@ describe '/lib/display_board.rb' do
     it 'prints an 3 cell row' do
       output = capture_puts{ display_board }
 
-      expect(output).to include("   |   |  ")
+      expect(output).to include("   |   |   ")
     end
 
     it 'separates rows with a line of 11 -' do
